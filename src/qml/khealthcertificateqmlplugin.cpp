a3147ee5909d061bfb269c94b54ecd433e9080ba
Volker Krause <vkrause@kde.org>
2021-10-23 13:22:49 +0200

    Expose vaccination state enums to QML

--- src/qml/khealthcertificateqmlplugin.cpp
+++ src/qml/khealthcertificateqmlplugin.cpp
@@ -50,6 +50,7 @@ void KHealthCertificateQmlPlugin::registerTypes(const char*)
 
     qmlRegisterUncreatableType<KHealthCertificate>("org.kde.khealthcertificate", 1, 0, "HealthCertificate", {});
     qmlRegisterUncreatableType<KTestCertificate>("org.kde.khealthcertificate", 1, 0, "TestCertificate", {});
+    qmlRegisterUncreatableType<KVaccinationCertificate>("org.kde.khealthcertificate", 1, 0, "VaccinationCertificate", {});
 
     // HACK qmlplugindump chokes on gadget singletons, to the point of breaking ecm_find_qmlmodule()
     if (QCoreApplication::applicationName() != QLatin1String("qmlplugindump")) {
